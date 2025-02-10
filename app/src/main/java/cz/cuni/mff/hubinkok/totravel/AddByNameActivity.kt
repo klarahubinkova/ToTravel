33fdee4b45f4f1e1af645705c484ff89ce3e36a8
Klára Hubínková <klara.hubinkova@volny.cz>
2025-02-10 12:59:50 +0100

    Add AddByNameActivity

---
+++ app/src/main/java/cz/cuni/mff/hubinkok/totravel/AddByNameActivity.kt
@@ -0,0 +1,11 @@
+package cz.cuni.mff.hubinkok.totravel
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AddByNameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_by_name)
+    }
+}
