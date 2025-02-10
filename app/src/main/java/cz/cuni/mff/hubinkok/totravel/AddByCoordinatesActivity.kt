499a4b2fed5feac79f7e8bba675bc533b5fd7582
Klára Hubínková <klara.hubinkova@volny.cz>
2025-02-10 14:28:11 +0100

    Add coordinate filters

--- app/src/main/java/cz/cuni/mff/hubinkok/totravel/AddByCoordinatesActivity.kt
+++ app/src/main/java/cz/cuni/mff/hubinkok/totravel/AddByCoordinatesActivity.kt
@@ -1,8 +1,11 @@
 package cz.cuni.mff.hubinkok.totravel
 
 import android.os.Bundle
+import android.text.InputFilter
+import android.text.Spanned
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 class AddByCoordinatesActivity : AppCompatActivity() {
@@ -10,6 +13,11 @@ class AddByCoordinatesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_by_coordinates)
+
+        val latitudeEditText = findViewById<EditText>(R.id.latitudeText)
+        latitudeEditText.filters = arrayOf<InputFilter>(MinMaxFilter(0, 90))
+        val longitudeEditText = findViewById<EditText>(R.id.longitudeText)
+        longitudeEditText.filters = arrayOf<InputFilter>(MinMaxFilter(0, 180))
 
         val latitude = arrayOf("N", "S")
         val longitude = arrayOf("E", "W")
@@ -27,6 +35,32 @@ class AddByCoordinatesActivity : AppCompatActivity() {
         longitudeDropdownMenu.setAdapter(longitudeAdapter)
         longitudeDropdownMenu.setOnItemClickListener { _, _, position, _ ->
             val selectedLongitude = longitude[position]
+        }
+    }
+
+    inner class MinMaxFilter() : InputFilter {
+        private var intMin: Int = 0
+        private var intMax: Int = 0
+
+        constructor(minValue: Int, maxValue: Int) : this() {
+            this.intMin = minValue
+            this.intMax = maxValue
+        }
+
+        override fun filter(source: CharSequence, start: Int, end: Int, dest: Spanned, dStart: Int, dEnd: Int): CharSequence? {
+            try {
+                val input = Integer.parseInt(dest.toString() + source.toString())
+                if (isInRange(intMin, intMax, input)) {
+                    return null
+                }
+            } catch (_: NumberFormatException) { }
+            return ""
+        }
+
+        // Check if input c is in between min a and max b and
+        // returns corresponding boolean
+        private fun isInRange(a: Int, b: Int, c: Int): Boolean {
+            return if (b > a) c in a..b else c in b..a
         }
     }
 }
